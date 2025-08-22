771439df76bbef3eecfbfe586ba5e285e372552e
ryanwidyafebiana-24 <widyariyan550@gmail.com>
2025-08-22 07:52:02 +0700

    Add files via upload

---
+++ XI TEKNIK INFORMATIKA/XI TEKNIK INFORMATIKA/Ryan Widya Febiana/Game Develoment/script.js
@@ -0,0 +1,10 @@
+let angkaAcak, percobaan, nyawa, timer;
+
+const tebakanInput = document.getElementById ('tebakanInput');
+const Masukan = document.getElementById ('masukkan');
+const mainLagi = document.getElementById ('mainLagi');
+const hasil = document.getElementById ('hasil');
+const nyawaDisplay = document.getElementById ('nyawa');
+const waktuDisplay = document.getElementById ('waktu');
+const highScoreDisplay = document.getElementById ('highScore');
+
